19bc1b005da086383fcf9b24319db3a9604bc967
Lois Harris <lois.harris@warnermedia.com>
2022-03-05 12:18:19 -0500

    Covert app.js to class component

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import { BrowserRouter,
  } from "react-router-dom";
 import OrganizationComponent from './Organization/organization.component'
 import RepoComponent from './Repo/repo.component';
-
-function App() {
-  return (
+import React from 'react';
+class App extends React.Component {
+  render() {
+    return (
       <div className="App">
       <BrowserRouter>
     <Routes>
-      {/* http://localhost:3001/ */}
       <Route path="/" element={<OrganizationComponent />} />
-      {/* http://localhost:3001/org/repo/ministrycentered */}
       <Route path="org/repo/:org" element={<RepoComponent />} />
     </Routes>
     </BrowserRouter>
+      
     </div>
   );
+}
 }
 
 export default App;
